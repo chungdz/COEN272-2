9418fb0da67cb0dd90d08ac2a4db2e3d332435ba
xiaoxin Hu <1033718596@qq.com>
2022-05-13 10:58:47 -0700

    first

---
+++ run.sh
@@ -0,0 +1,6 @@
+mkdir data
+cd data
+wget https://www.cse.scu.edu/~yfang/coen272/train.txt
+wget https://www.cse.scu.edu/~yfang/coen272/test5.txt
+wget https://www.cse.scu.edu/~yfang/coen272/test10.txt
+wget https://www.cse.scu.edu/~yfang/coen272/test20.txt
